e0d3d1fd179a3ad231d65367f2b038e95c08bbce
JongSoo Park <newtein80@gmail.com>
2018-11-13 02:07:16 +0900

    2018.11.13.Home Commit - asp.net core applicationuser model class not

--- PARK.JONG.SOO/Models/ApplicationModel/ApplicationUser.cs
+++ PARK.JONG.SOO/Models/ApplicationModel/ApplicationUser.cs
@@ -8,7 +8,11 @@ namespace PARK.JONG.SOO.Models.ApplicationModel
     // InvalidOperationException: Unable to resolve service for type 'Microsoft.AspNetCore.Identity.SignInManager`1[Microsoft.AspNetCore.Identity.IdentityUser]' while attempting to activate
     public class ApplicationUser : IdentityUser
     {
-        [Display(Name = "xxxx122")]
+        [Display(Name = "USER NAME")]
         override public string UserName { get; set; }
+
+        // override 작성한 뒤 ctrl + . 누르면 오버라이드 대상목록이 나옴. 선택하면 자동으로 작성됨
+        [Display(Name = "E-MAIL")]
+        public override string Email { get => base.Email; set => base.Email = value; }
     }
 }
